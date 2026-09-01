bf8bee2aa9ea759c4ed6162264a68d6cc392ea7e
Carlos Murilo de O. C. F. <murilojagger@gmail.com>
2026-08-31 21:44:16 -0300

    refactor: centralize input validation and error logging

--- mandelbrot.c
+++ mandelbrot.c
@@ -1,57 +1,64 @@
-#include <_time.h>
-#include <pthread.h>
+#include <errno.h>
+#include <limits.h>
+#include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h> // atoi(), strtol()
+#include <stdlib.h>
 
-// Entrada
-typedef struct Params {
-  int largura;       // argv[1]
-  int altura;        // argv[2]
-  int max_iteracoes; // argv[3]
-  int num_threads;   // argv[4]
-} Params;
+int width;
+int height;
+int max_iterations;
+int number_of_threads;
+int *image;
+FILE *error_file;
+
+void save_error(const char *message) { fprintf(error_file, "%s\n", message); }
+
+int read_number(const char *text, int *number) {
+  char *end;
+  long value;
+
+  errno = 0;
+  value = strtol(text, &end, 10);
+
+  if (errno != 0 || *text == '\0' || *end != '\0' || value <= 0 ||
+      value > INT_MAX) {
+    return 0;
+  }
+
+  *number = (int)value;
+  return 1;
+}
 
 int main(int argc, char *argv[]) {
+  error_file = fopen("errors.txt", "w");
+  if (error_file == NULL) {
+    return EXIT_FAILURE;
+  }
 
   if (argc != 5) {
-    fprintf(stderr,
-            "Use: <largura>, <altura>, <max_iteracoes> <num_threads>\n");
-
-    return 1;
+    save_error("Uso: mandelbrot largura altura max_iteracoes num_threads");
+    return EXIT_FAILURE;
   }
 
-  Params p1;
-
-  p1.largura = atoi(argv[1]);
-  if (p1.largura <= 0) {
-    fprintf(stderr, "Largura: tem que ser maior que 0\n");
-    return 1;
+  if (!read_number(argv[1], &width) || !read_number(argv[2], &height) ||
+      !read_number(argv[3], &max_iterations) ||
+      !read_number(argv[4], &number_of_threads)) {
+    save_error("Erro: os quatro argumentos devem ser inteiros positivos.");
+    return EXIT_FAILURE;
   }
 
-  p1.altura = atoi(argv[2]);
-  if (p1.altura <= 0) {
-    fprintf(stderr, "Altura: tem que ser maior que 0\n");
-    return 1;
+  if ((size_t)width > SIZE_MAX / sizeof(int) / (size_t)height) {
+    save_error("Erro: as dimensoes da imagem sao muito grandes.");
+    return EXIT_FAILURE;
   }
 
-  int *buffer = malloc(p1.altura * p1.largura * sizeof(int));
-
-  if (buffer == NULL) {
-    fprintf(stderr, "Erro ao alocar memória");
-    return 1;
+  image = malloc((size_t)width * height * sizeof(int));
+  if (image == NULL) {
+    save_error("Erro ao alocar memoria para a imagem.");
+    return EXIT_FAILURE;
   }
 
-  p1.max_iteracoes = atoi(argv[3]);
-  if (p1.max_iteracoes <= 0) {
-    fprintf(stderr, "Max_iteracoes: tem que ser maior que 0\n");
-    return 1;
-  }
-
-  p1.num_threads = atoi(argv[4]);
-  if (p1.num_threads <= 0) {
-    fprintf(stderr, "Num_threads: tem que ser maior que 0\n");
-    return 1;
-  }
-
-  return 0;
+  free(image);
+  fclose(error_file);
+  return EXIT_SUCCESS;
 }
